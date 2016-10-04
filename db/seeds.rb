9472262bf9fc0d461839befcad6e682aa2a96292
Chris <chris@adadevelopersacademy.org>
2016-10-03 18:29:06 -0700

    Added Seed Example.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.foreach('seeds_csvs/posts.csv', :headers => true ) do |csv_obj|
+	Post.create(id: csv_obj['id'].to_i, title: csv_obj['title'], body: csv_obj['body'], author: csv_obj['author'])
+end 
